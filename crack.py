f4f1b74a37f2c431c791bf9857cfa0b51c9960f0
Anja-dhnd <anais.dehand@gmail.com>
2021-10-30 20:25:34 +0000

    finished script

--- crack.py
+++ crack.py
@@ -1,5 +1,8 @@
 import hashlib
 
+flag = 0
+counter = 0
+
 pass_hash = input("Enter md5 hash: ")
 
 wordList = input("File name: ")
@@ -14,7 +17,18 @@ except:
     enc_wrd = word.encode('utf-8')
     digest = hashlib.md5(enc_wrd.strip()).hexdigest()
 
+    # print(word)
+    # print(digest)
+    # print(pass_hash)
+
+counter += 1
+
+
     if digest == pass_hash:
         print("Password found")
         print("It is " + word)
+        flag = 1
         break
+
+if flag == 0:
+    print("password has not been found")
